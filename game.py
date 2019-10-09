517b608ea387269d1a45bd4bde48e165f631df72
lesliehealray <lesliehealray@gmail.com>
2019-10-09 19:42:03 -0400

    initial commit, need to finish work on enumerate/handle ending

--- game.py
+++ game.py
@@ -1,67 +1,78 @@
 from collections import defaultdict
-import random
+from random import choice
 
 
-def mystery_word(filename):
+
+def word_list():
     with open("words.txt") as file_handler:
         words = file_handler.read().split()
-        #remove regex?
     return words
     
 
-def game_level(words):
-    mode = defaultdict(list)
+def game_levels(words):
+    levels = defaultdict(list)
     for word in words:
         if len(word) <= 6:
-            mode['EASY'].append(word)
+            levels['EASY'].append(word)
         elif 8 > len(word) > 6:
-            mode['MODERATE'].append(word)
+            levels['MODERATE'].append(word)
         else:
-            mode['HARD'].append(word) 
-    return mode 
+            levels['HARD'].append(word) 
+        
+    return levels
+    
+def set_difficulty(levels):
+    keys = levels.keys()
+    level_set = False
+    while not level_set:
+        difficulty = input("Welcome to Mystery Word Challenge! Do you want an EASY, MODERATE or HARD word to guess? ").upper()
+        level_set = difficulty in keys
+    return difficulty
 
-   
-def begin_game():
-    get_level = raw_input("Welcome to Mystery Word Challenge! Do you want an EASY, MODERATE or HARD word to guess?")
-    requested _level = get_level.upper()
-    if requested_level == "EASY" or "MODERATE" or "HARD":
-        pick_word()
-    elif not requested_level:
-        return get_level
-    else print("Please choose EASY, MODERATE or HARD")
-
-
-
-
-def pick_word(mode):
-
-    secret_word = random.choice.get(level)
+def find(word, letter):
+    # loops through every letter in the word and compares it to the submitted letter. 
+    # returns indexes for each occurance of the letter in the word.
+    return [i for i, x in enumerate(word) if x == letter]
 
 
 
+def prepare_game():
+    words = word_list()
+    levels = game_levels(words)
+    difficulty = set_difficulty(levels)
+    mystery_word = choice(levels[difficulty]) 
+    return mystery_word
 
 
+def run_game():
+    mystery_word = prepare_game()
+    guesses = 0
+    wrong_guesses = []
+    board = "_" * len(mystery_word)
+    while guesses <= 10:
+        letter = input('Guess a letter: ')
+        if not letter.isalpha() and len(letter) != 1:
+            print("Please submit a single letter.")
+        elif board == mystery_word:
+            print("you won!")
+        else:
+            guesses +=1
+            if letter in mystery_word:
+                index = mystery_word.index(letter) 
+                board = list(board)
+                matches = find()
+                board[index] = letter 
+                board = "".join(board) 
+            else:
+                wrong_guesses.append(letter)
+                print(f"Wrong Guesses {wrong_guesses}")
+            print(board)          
+    else:
+        print(f"You lose the word was {mystery_word}")
+    
 
-
-
-
-
-
-
+    
  
 if __name__ == "__main__":
-    import argparse
-    from pathlib import Path
-
-    parser = argparse.ArgumentParser(
-        description='Guess the mystery word.')
-    parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
-
-    file = Path(args.file)
-    if file.is_file():
-        words = mystery_word(file)
-        words_by_difficulty = game_level(words)
-    else:
-        print(f"{file} does not exist!")
-        exit(1)
+    
+    run_game()
